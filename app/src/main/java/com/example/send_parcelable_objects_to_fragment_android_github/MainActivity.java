4fc980dbba6058db2d4ae4a023cf6b5a34ed2d1f
Aman Sharma <amansharma.11@hotmail.com>
2019-06-28 18:17:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/send_parcelable_objects_to_fragment_android_github/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.send_parcelable_objects_to_fragment_android_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
